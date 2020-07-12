51a138d87ca3f937c0dfff0466a1da92b1998ce8
mandymv <mandymvarley@gmail.com>
2020-07-12 16:27:59 -0400

    Removed unrelevant text and added NameForm view

--- Mandy1app/views.py
+++ Mandy1app/views.py
@@ -1,9 +1,6 @@
 from django.views.generic import TemplateView
 from django.views.generic import FormView
-from .forms import NameForm
-
-class Home(TemplateView):
-    template_name = "index.html"
+from Mandy1app.forms import NameForm
 
 class FillOut(FormView):
     template_name = "form.html"
@@ -11,14 +8,11 @@ class FillOut(FormView):
     success_url = "thanks/"
 
     def post(self, request, *args, **kwargs):
-        print('hello')
-        print(self.request.POST)
-        print('hello')
-        print('hello')
-        print(self.request.POST['first_name'])
-
         form = self.get_form()
         if form.is_valid():
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
+
+class Home(TemplateView):
+    template_name = "index.html"
